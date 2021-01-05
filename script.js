7abc1aa62b3a5d7e074d493d81aac60b42733d39
Manjinder <manjindersinghjohal@aol.com>
2021-01-06 00:10:49 +0530

    Add and clear functionality added

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-console.log('script is working!');
+const inputElement = document.getElementById('input');
+const listElement = document.getElementById('list');
+let list = [];
+
+listElement.innerHTML = 'Your To-Do list is empty!';
+
+const addItem = () => {
+    list.push(inputElement.value);
+    listElement.innerHTML = list;
+}
+
+const clearAll = () => {
+    list = [];
+    listElement.innerHTML = 'Your To-Do list is empty!';
+}
